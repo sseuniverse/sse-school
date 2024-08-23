cfcaca4ed3fa092efa9f0f57ebdb119e49e330e6
SSE World <suryakantmaurya2007@gmail.com>
2024-08-23 18:04:09 +0530

    Commit 4
    Components
    
    Co-Authored-By: SSE World <114757626+sseworld@users.noreply.github.com>
    Co-Authored-By: SSE Official <sseworld4@gmail.com>

--- react/frontend/src/components/table/index.jsx
+++ react/frontend/src/components/table/index.jsx
@@ -4,7 +4,7 @@ export { default as useTable } from './useTable';
 
 export { default as TableNoData } from './TableNoData';
 export { default as TableSkeleton } from './TableSkeleton';
-export { default as TableEmptyRows } from './TableEmptyRows';
+export { default as TableEmptyRows } from './TableEmptyRows.jsxx';
 export { default as TableHeadCustom } from './TableHeadCustom';
 export { default as TableSelectedAction } from './TableSelectedAction';
 export { default as TablePaginationCustom } from './TablePaginationCustom';
